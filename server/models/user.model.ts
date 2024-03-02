d99fcb978c824f754bf855a549ec02daeb0b47c3
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-02 23:11:39 +0530

    login module added

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,5 +1,7 @@
+require("dotenv").config();
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const emailRegexpattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,6 +17,8 @@ export interface IUser extends Document {
     isVerified: boolean;
     courses: Array<{ courseId: string }>;
     comparePassword: (password: string) => Promise<boolean>;
+    SignAccessToken: () => string;
+    SignRefeshToken: () => string;
 };
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
@@ -64,6 +68,16 @@ userSchema.pre<IUser>("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
+
+// sign access token
+userSchema.methods.SignAccessToken = function (){
+    return jwt.sign({id: this._id}, process.env.ACCESS_TOKEN || "");
+}
+
+// sign refresh token
+userSchema.methods.SignRefeshToken = function (){
+    return jwt.sign({id: this._id}, process.env.REFRESH_TOKEN || "");
+}
 
 // compare password
 userSchema.methods.comparePassword = async function (enteredPassword: string): Promise<boolean> {
