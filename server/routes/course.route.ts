9faf502f1443509866f329b2e5bd6a93b50ac8b0
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-05 18:40:42 +0530

    added get single and all courses - without purchasing

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRole, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
 courseRouter.post("/create-course", isAuthenticated, authorizeRole("admin"), uploadCourse);
 courseRouter.put("/edit-course/:id", isAuthenticated, authorizeRole("admin"), editCourse);
+courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourses);
 
 export default courseRouter;
