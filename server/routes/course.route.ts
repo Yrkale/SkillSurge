58c1a69459364c054732600708d59f39a2302c43
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-06 00:44:52 +0530

    added create question and reply feature

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addAnswer, addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRole, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -8,5 +8,7 @@ courseRouter.put("/edit-course/:id", isAuthenticated, authorizeRole("admin"), ed
 courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 courseRouter.get("/get-course-content/:id",isAuthenticated, getCourseByUser);
+courseRouter.put("/add-question",isAuthenticated, addQuestion);
+courseRouter.put("/add-answer",isAuthenticated, addAnswer);
 
 export default courseRouter;
