7ff0f8fbbb1ed3eac3608db44472c6239219b555
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-05 14:40:51 +0530

    added update password feature

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updateUserInfo } from "../controllers/user.controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateUserInfo } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
@@ -11,6 +11,8 @@ userRouter.get("/refresh", updateAccessToken);
 userRouter.get("/me", isAuthenticated, getUserInfo);
 userRouter.post("/social-auth", socialAuth);
 userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+
 
 
 export default userRouter;
