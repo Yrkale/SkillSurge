a55a2254b4892c34ddc5f6b70446f841120f5cd1
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-08 20:24:11 +0530

    added delete notification feature

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import NotificationModel from "../models/notification.Model";
 import { NextFunction, Response, Request } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
+import cron from "node-cron";
 
 // get all notifications -- only for admin
 export const getNotifications = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
@@ -41,3 +42,9 @@ export const updateNotification = CatchAsyncError(async (req: Request, res: Resp
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+// delete notification - only admin
+cron.schedule("0 0 0 * * *", async () => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    await NotificationModel.deleteMany({ status: "read", createdAt: { $lt: thirtyDaysAgo } });
+})
