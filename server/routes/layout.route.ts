fed361f100c693f256de8cd0c564bf676c64ffdd
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-10 05:10:45 +0530

    added update faq

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { authorizeRole, isAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
 const layoutRouter = express.Router();
 
 layoutRouter.post("/create-layout", isAuthenticated, authorizeRole("admin"), createLayout);
+layoutRouter.put("/edit-layout", isAuthenticated, authorizeRole("admin"), editLayout);
+layoutRouter.get("/get-layout", getLayoutByType);
 
 export default layoutRouter;
