bc8ec07c959876e584ec69ba99b93251bc0ea4ba
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-03-10 05:28:24 +0530

    cache maintainance

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -178,9 +178,9 @@ export const updateAccessToken = CatchAsyncError(async (req: Request, res: Respo
             return next(new ErrorHandler(message, 400));
         }
         const session = await redis.get(decoded.id as string);
+        
         if (!session) {
-            return next(new ErrorHandler(message, 400));
-
+            return next(new ErrorHandler("Please login for access this resources", 400)); 
         }
 
         const user = JSON.parse(session);
@@ -197,6 +197,8 @@ export const updateAccessToken = CatchAsyncError(async (req: Request, res: Respo
 
         res.cookie("access_token", accessToken, accessTokenOptions);
         res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+        await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7 days
 
         res.status(200).json({
             status: "success",
@@ -409,7 +411,7 @@ export const deleteUser = CatchAsyncError(async (req: Request, res: Response, ne
 
         res.status(200).json({
             success: true,
-            message:"User deleted successfully"
+            message: "User deleted successfully"
         });
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 400));
